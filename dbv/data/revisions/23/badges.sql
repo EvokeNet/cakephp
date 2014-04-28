18a4d0bcbd05056fd2e9d9da063108080a299450
Alexandre Alvares <alexandre@alexandre.(none)>
2014-04-28 11:25:48 -0300

    problem in revision

--- dbv/data/revisions/23/badges.sql
+++ dbv/data/revisions/23/badges.sql
@@ -1,1 +1,2 @@
-ALTER TABLE  `badges` ADD  `name_es` VARCHAR( 120 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER  `name`, ALTER TABLE  `badges` ADD  `description_es` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER  `description`;
+ALTER TABLE  `badges` ADD  `name_es` VARCHAR( 120 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `name`, 
+ALTER TABLE  `badges` ADD  `description_es` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `description`;
