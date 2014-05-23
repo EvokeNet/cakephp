44a3f51ffd413af94487a8abd0a27bccb536a40d
Alexandre Alvares <alexandre@alexandre.(none)>
2014-05-23 17:52:37 -0300

    limiting evidences in profile

--- Controller/UsersController.php
+++ Controller/UsersController.php
@@ -646,10 +646,11 @@ class UsersController extends AppController {
 
 		$myevidences = $this->User->Evidence->find('all', array(
 			'order' => array(
-				'Evidence.created DESC'
+				'Evidence.modified DESC'
 			),
 			'conditions' => array(
 				'Evidence.user_id' => $id,
+				'Evidence.title != ' => ''
 			)
 		));
 
