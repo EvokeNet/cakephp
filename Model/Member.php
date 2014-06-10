ebc25c412e1ae94d9937469aa294b410cc675f8a
Alexandre Alvares <alexandre@alexandre.(none)>
2014-06-10 16:08:26 -0300

    preparing db and models to chat implementation

---
+++ Model/Member.php
@@ -0,0 +1,58 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Member Model
+ *
+ * @property ChatConversation $ChatConversation
+ * @property User $User
+ * @property Message $Message
+ */
+class Member extends AppModel {
+
+
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+
+/**
+ * belongsTo associations
+ *
+ * @var array
+ */
+	public $belongsTo = array(
+		'ChatConversation' => array(
+			'className' => 'ChatConversation',
+			'foreignKey' => 'chat_conversation_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		),
+		'User' => array(
+			'className' => 'User',
+			'foreignKey' => 'user_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		)
+	);
+
+/**
+ * hasMany associations
+ *
+ * @var array
+ */
+	public $hasMany = array(
+		'Message' => array(
+			'className' => 'Message',
+			'foreignKey' => 'member_id',
+			'dependent' => false,
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'exclusive' => '',
+			'finderQuery' => '',
+			'counterQuery' => ''
+		)
+	);
+
+}
