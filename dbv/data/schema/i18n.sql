2b97da982b48c741d6beb3303f9a832dffef2499
Renata Japur <rjapur@quanti.ca>
2014-09-30 18:28:40 -0300

    tabela i18n

---
+++ dbv/data/schema/i18n.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `i18n` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `locale` varchar(6) NOT NULL,
+  `model` varchar(255) NOT NULL,
+  `foreign_key` int(10) NOT NULL,
+  `field` varchar(255) NOT NULL,
+  `content` text,
+  PRIMARY KEY (`id`),
+  KEY `locale` (`locale`),
+  KEY `model` (`model`),
+  KEY `row_id` (`foreign_key`),
+  KEY `field` (`field`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
