822dd956aa081ef91c70eeb5a1a3bb6b73e7ab09
Gabi <gscardine@quanti.ca>
2015-03-16 17:08:13 -0300

    tentativa de consertar o tamanho do sidr depois de fechado

--- webroot/js/requirejs/app/Common/view-mission.js
+++ webroot/js/requirejs/app/Common/view-mission.js
@@ -79,8 +79,13 @@ require([webroot+'js/requirejs/bootstrap'], function () {
 				$('#missions-content-overlay').addClass("hidden"); //Hide content overlay
 				$('.right-small').removeClass("open"); //Off-canvas buttons go back to the right
 
+				//Go to the top of the page
+				$("html, body").animate({
+					scrollTop: 0
+				}, 300);
+
 				//Reset min-height that might have changed when sidr was opened
-	    		var sliderHeight = $(".slick-active").height(); //Height of the carousel image that will be displayed
+	    		var sliderHeight = $(".slick-active img").height(); //Height of the carousel image that will be displayed
 	    		$(".off-canvas-wrap").css("min-height",sliderHeight).css("height",sliderHeight);
 	    		$(sidr_source).css("min-height",sliderHeight).css("height",sliderHeight);
 	    		$('#missions-content-overlay').css("min-height",sliderHeight).css("height",sliderHeight);
