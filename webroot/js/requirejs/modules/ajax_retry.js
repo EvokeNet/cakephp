32007fef86dbaac443149a5e9448f91cc1e228e8
gscardine <gscardine@quanti.ca>
2015-05-29 18:03:25 -0300

    Improved comments

--- webroot/js/requirejs/modules/ajax_retry.js
+++ webroot/js/requirejs/modules/ajax_retry.js
@@ -1,17 +1,22 @@
 define(['jquery','sweetalert', 'jqueryajaxretry'], function($,swal){
 	$.ajaxPrefilter(function(opts, originalOpts, jqXHR) {
+		//Avoiding infinite loop in case the same request is refreshed
 		if (opts.refreshRequest) {
 			return;
 		}
 
 		opts.refreshRequest = true;
 
+		//If retrying is not specified in the ajax request, this is the default behavior
 		if(!opts.times) {
+			//Trying 3 times, with the jquery ajax retry plugin
 			jqXHR.retry({times:3})
 				.fail(function(){
+					//Session expired
 					if (jqXHR.status === 401) {
 						swal("Oops...", 'Your session has expired. Please sign in again.', "error");
 					}
+					//General errors
 					else {
 						swal("Oh no!", 'Sorry, we could not retrieve this page. Please try again.', "error");
 					}
