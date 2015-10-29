96aaa6f112ddfe56b9ec19dcfa31cf960fbaeb12
JayantGandhi <jayant.gandhi127@gmail.com>
2015-10-29 17:43:41 -0400

    joyride minus scrolling

--- webroot/js/requirejs/app/Elements/Missions/basic_training_tour.js
+++ webroot/js/requirejs/app/Elements/Missions/basic_training_tour.js
@@ -1,5 +1,9 @@
 require([webroot+'js/requirejs/bootstrap'], function () {
 	require(['jquery', 'foundation', 'foundationjoyride'], function() {
-		$(document).foundation('joyride', 'start');
+		$(document).on('ready', function() {
+			$(document).foundation('joyride', 'start', {
+				scroll_speed : 0
+			});
+		});
 	});
 });
